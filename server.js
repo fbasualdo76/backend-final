aba8e0d9e40045e374113e3a4b6a647b092adfe4
FRANCO <fbasualdo76@gmail.com>
2024-07-13 21:30:23 -0300

    prueba de vida.

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ const app = express()
 app.use(cors())//El backend al que estamos consultando no está hecho para recibir consultas. Permitimos consulta de origen cruzado.
 app.use(express.json())//permite que mi aplicación reciba json.
 
+app.get('/test', (req, res) => {
+    res.json({ status: 200, message: 'HOLIS' })
+})
+
 app.use('/api/auth', authRouter)//indico la ruta que va utilizar el authRouter. Que mi aplicación utilice la ruta `api/auth` con los endpoints que estan en el auth.router.js
 app.use('/api/products', productsRouter)//indico la ruta que va utilizar el productsRouter. Que mi aplicación utilice la ruta `api/products` con los endpoints que estan en el products.router.js
 app.use('/api/carts', cartsRouter)//indico la ruta que va utilizar el cartsRouter. Que mi aplicación utilice la ruta `api/carts` con los endpoints que estan en el carts.router.js
